180e0787dfb557ba5b4fc86de245bba4ae2e7a4d
vagamerous <74257480+vagamerous@users.noreply.github.com>
2021-04-15 23:43:24 -0300

    Update freeCodeCampProject3.py

--- freeCodeCampProject3.py
+++ freeCodeCampProject3.py
@@ -1,33 +1,44 @@
 class Category:
 
-  ledger = []
-  balance = 0
+  ledger: list = []
+  balance: float = 0
 
-  def __init__(self, category):
-    self.__category = category
-    self.__balance = Category.balance
-    self.__ledger = Category.ledger
+  def __init__(self, category: str):
+    self.__category: str = category
+    self.__balance: float = Category.balance
+    self.__ledger: list = Category.ledger
 
-  def deposit(self, amount, description="None"):
+  def check_funds(self, amount):
+    if float(amount) > self.__balance:
+      return False
+    else:
+      return True
+
+  def deposit(self: object, amount: float, description="None"):
     Category.ledger.append({'amount': amount, 'description': description})
-    self.__balance = Category.balance + float(amount)
+    self.__balance: float = Category.balance + float(amount)
 
   def withdraw(self, amount, description='None'):
-    if float(amount) < self.__balance:
+    x = Category.check_funds(self, amount)
+    if x == True:
       Category.ledger.append({'amount': amount, 'description': description})
-      self.__balance =  float(amount)
+      self.__balance = self.__balance - float(amount)
       return True
     else:
       return False
 
-  def get_balance(self):
-    print(f'{self.__balance}')
-    return self.__balance
+  def get_balance(self) -> str:
+    return f'O saldo é {self._Category__balance}'
 
-  def transfer(self, amount, category):
-    if float(amount) < self.__balance:
-      Category.withdraw(self, amount, "Transfer to {category}")
-      Category.deposit(category, amount, "Transfer from {self.__category}")
+  # Retira do saldo da categoria atual (self) e deposita em outra categoria (anotther_category)
+  def transfer(self, amount, another_category):  
+    if Category.check_funds(self, amount) == True:
+      Category.withdraw(self, amount, "Transfer to f'{another_category}'")
+      Category.deposit(another_category, amount, "Transfer from {self.__category}")
       return True
     else:
       return False
+
+  def __str__(self: object) -> str:
+    return f'{self.__category:*^30}'
+
