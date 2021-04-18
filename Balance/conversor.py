a3bf1fee61ba16aa6e516616820f3b7b0e24dabe
vagamerous <74257480+vagamerous@users.noreply.github.com>
2021-04-17 23:20:06 -0300

    Add files via upload

---
+++ Balance/conversor.py
@@ -0,0 +1,2 @@
+def converte_amount_str(amount: float) -> str:
+   return f'R$ {amount}'
---
+++ Balance/freeCodeCampProject3.py
@@ -0,0 +1,54 @@
+from convert_amount_str import convert_amount_str
+
+
+class Category:
+
+  ledger: list = []
+  balance: float = 0
+
+  def __init__(self, category: str):
+    self.__category: str = category
+    self.__balance: float = Category.balance
+    self.__ledger: list = Category.ledger
+
+  def check_funds(self, amount):
+    if float(amount) > self.__balance:
+      return False
+    else:
+      return True
+
+  def deposit(self: object, amount: float, description="None"):
+    Category.ledger.append({'amount': amount, 'description': description})
+    self.__balance: float = Category.balance + float(amount)
+
+  def withdraw(self, amount, description='None'):
+    x = Category.check_funds(self, amount)
+    if x == True:
+      Category.ledger.append({'amount': amount, 'description': description})
+      self.__balance = self.__balance - float(amount)
+      return True
+    else:
+      return False
+
+  def get_balance(self) -> str:
+    return f'O saldo é {self._Category__balance}'
+
+  # Retira do saldo da categoria atual (self) e deposita em outra categoria (anotther_category)
+  def transfer(self, amount, another_category):  
+    if Category.check_funds(self, amount) == True:
+      Category.withdraw(self, amount, "Transfer to f'{another_category}'")
+      Category.deposit(another_category, amount, "Transfer from {self.__category}")
+      return True
+    else:
+      return False
+
+
+
+  def formatacao(self: object) -> None:
+    for element in self.ledger:
+      print('{: <23}'.format(element['description']), '{: >7}'.format(convret_amount_str(element['amount'])))
+  
+  def __str__(self: object):  
+    
+      return self.formatacao()
+  
